a9d74c7d5794665374482bfaac5b81c310dce6b7
Frédéric Vachon <fredvac@gmail.com>
2015-06-12 11:18:23 -0400

    Added remote drush support in development

--- container/alignak/drupal.aliases.drushrc.php
+++ container/alignak/drupal.aliases.drushrc.php
@@ -2,7 +2,12 @@
 $aliases['drupal'] = array (
   'uri' => 'drupal',
   'root' => '/var/www/html/',
-  'remote-user' => 'root',
-  'remote-host' => 'drupal'
+  'remote-user' => 'nagios',
+  'remote-host' => 'drupal',
+  'path-aliases' =>
+  array (
+    '%drush' => '/home/nagios/.composer/vendor/drush/drush',
+    '%site' => 'sites/default/'
+  )
 )
 ?>
